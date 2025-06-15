a9b464a607bb044da9bbed6a27593cf07efbc8d9
ThanhHieuCSS <thanhhieule66@gmail.com>
2025-06-16 02:30:26 +0700

    upload lan 1

---
+++ script.js
@@ -0,0 +1,47 @@
+const qrCodeRegionId = "reader";
+const sheetURL = "https://script.google.com/macros/s/AKfycbx04-GtmrMIZAZI2E3g5i26F6x2yla5HW9SStqLavz_8rAUP6nuAgB08F42o792xE65MQ/exec";
+const beepSound = document.getElementById("beep");
+const readerEl = document.getElementById("reader");
+
+
+
+
+function onScanSuccess(decodedText, decodedResult) {
+  document.getElementById("qrText").innerText = decodedText;
+  document.getElementById("status").innerText = "📤 Đang gửi dữ liệu...";
+  
+    // 🔊 Phát tiếng bíp
+    beepSound.play();
+
+    // Hiện viền đỏ
+    readerEl.classList.add("qr-highlight");
+
+      // 🛑 Dừng quét ngay
+    html5QrCode.pause();
+
+  fetch(sheetURL, {
+    method: "POST",
+    body: new URLSearchParams({ data: decodedText })
+  })
+  .then(res => res.text())
+  .then(result => {
+    document.getElementById("status").innerText = "✅ Đã gửi: " + decodedText;
+    // 🕒 Tạm dừng quét trong 1 giây
+    setTimeout(() => {
+      readerEl.classList.remove("qr-highlight");
+      html5QrCode.resume();
+      document.getElementById("status").innerText = "⏳ Đang chờ quét...";
+    }, 500);
+  })
+  .catch(error => {
+    document.getElementById("status").innerText = "❌ Lỗi gửi dữ liệu!";
+    console.error("Lỗi:", error);
+  });
+}
+
+const html5QrCode = new Html5Qrcode(qrCodeRegionId);
+html5QrCode.start(
+  { facingMode: "environment" },
+  { fps: 10, qrbox: 250 },
+  onScanSuccess
+);
