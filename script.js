6c62e3354de890eb71f1096fe317bef4bd597a6d
ThanhHieuCSS <thanhhieule66@gmail.com>
2025-06-17 02:35:40 +0700

    camera khung

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ const html5QrCode = new Html5Qrcode(qrCodeRegionId);
 html5QrCode.start(
   { facingMode: "environment" },
   { fps: 10,
-    qrbox: { width: 350, height: 350 }//khung quét QR
+    qrbox: { width: 300, height: 200 }//khung quét QR
   },
   onScanSuccess
 );
